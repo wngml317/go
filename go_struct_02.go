9d7fc432424edad14c44140245b2bb8286bb0c83
root <wngml317@naver.com>
2020-11-29 14:30:53 +0900

    v2 struct

--- go_struct_02.go
+++ go_struct_02.go
@@ -22,6 +22,10 @@ func defaultSubscriber(name string) subscriber {
 	return s
 }
 
+func applyDiscount(s *subscriber)  {
+	s.rate = 4000
+}
+
 func main()  {
 	s1 := defaultSubscriber("kim")
 	s1.rate = 4500
@@ -29,4 +33,8 @@ func main()  {
 
 	s2 := defaultSubscriber("park")
 	printInfo(s2)
+
+	var s3 subscriber
+	applyDiscount(&s3)
+	fmt.Println(s3.rate)
 }
